2ca50dd90fe349b58c9c4971d2a3807f22456c22
Sarah Zajac <sarahmzajac@gmail.com>
2018-08-07 16:07:48 -0400

    adding stuff

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Hello extends Component{
+  render(){
+    return(
+      <div>
+        hello
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +23,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Hello />
       </div>
     );
   }
